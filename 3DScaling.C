31311eb75d1f40f0f658eee5be55a8cc3bff0ed4
DMG <upendmg@users.noreply.github.com>
2016-05-02 18:44:23 +0530

    3dTransformation of any object

---
+++ 3DScaling.C
@@ -0,0 +1,28 @@
+#include<graphics.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int gdriver=DETECT,gmode,errorcode;
+int midx,midy,i,sx,sy,sz;
+initgraph(&gdriver,&gmode,"C:\\turboc3\\bgi");
+errorcode=graphresult();
+if (errorcode != grOk)  /* an error occurred */
+{
+printf("Press any key to halt:");
+getch();
+exit(1);             /* return with error code */
+}
+midx=getmaxx()/2;
+midy=getmaxy()/2;
+bar3d(midx-50,midy-50,midx+50,midy+50,10,1);
+getch();
+cleardevice();
+printf("\nEnter sx,sy,sz:");
+scanf("%d%d%d",&sx,&sy,&sz);
+bar3d(midx-50*sx,midy-50*sy,midx+50*sx,midy+50*sy,10*sx,1);
+getch();
+closegraph();
+return 0;
+}
